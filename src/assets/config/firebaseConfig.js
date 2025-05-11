6a2c59b20a0182b544bf20cd9f11ee3660feb117
the-icemann <andrewssuubi@gmail.com>
2025-05-11 16:04:25 +0300

    Added a .env file to secure firebase credentials

--- src/assets/config/firebaseConfig.js
+++ src/assets/config/firebaseConfig.js
@@ -2,24 +2,25 @@
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
+
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCGUvMOZhsIWGydMZjFN3x1AkU38BHOcNM",
-  authDomain: "admit-9e216.firebaseapp.com",
-  projectId: "admit-9e216",
-  storageBucket: "admit-9e216.firebasestorage.app",
-  messagingSenderId: "293875761031",
-  appId: "1:293875761031:web:b68aab5eb1aaccaf531d22",
+  apiKey:import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain:import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket:import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
   measurementId: "G-D2LG1V986M"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+// const analytics = getAnalytics(app);
 export const auth =getAuth(app);
 export const googleProvider=new GoogleAuthProvider();
 
