8503e6b9addae3b739e3fecfd8a8a4af1ebcfda3
the-icemann <andrewssuubi@gmail.com>
2025-05-27 11:52:47 +0300

    Added new firebase config, gsap as well

--- src/assets/config/firebaseConfig.js
+++ src/assets/config/firebaseConfig.js
@@ -24,5 +24,6 @@ const app = initializeApp(firebaseConfig);
 // const analytics = getAnalytics(app);
 export const auth =getAuth(app);
 export const googleProvider=new GoogleAuthProvider();
+
 export const db= getFirestore(app)
 
